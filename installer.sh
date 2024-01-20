92bf682907a42aa3f45ee2f53a4420ef4354c8e8
Lazialize <lazinelia@gmail.com>
2024-01-20 16:20:49 +0900

    feat(installer): Add installer.sh
    this file is work in progress! cannot execute.

---
+++ installer.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -u
+
+helpmsg() {
+  command echo "Usage: $0 [...options]" 0>&2
+  command echo ""
+  command echo "[Options]"
+  command echo "--help, -h: Shows this message"
+  command echo ""
+}
+
+check_os() {
+  if [ "$(uname)" == "Darwin" ]; then
+    OS="Mac"
+  else
+    command echo "Cannot recognize your operating system."
+    command echo "So, dotfiles installing process is terminated."
+    command echo ""
+    exit 1
+  fi
+}
+
+install_brew() {
+  command echo "Install homebrew process started."
+  brew -v >> /dev/null
+  
+  if [ $? -ne 0 ]; then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+    export PATH=$PATH:/opt/homebrew/bin
+  else
+    command echo "The homebrew is already installed. So, the process is skipped."
+  fi
+}
+
+while [ $# -gt 0 ]
+do
+  case ${1} in
+    --help|-h)
+      helpmsg
+      exit 0
+      ;;
+    *)
+      ;;
+  esac
+  shift
+done
+
+check_os
+command echo "Your operation system is recognized: ${OS}"
+
+if [[ $OS == 'Mac' ]]; then
+  install_brew
+fi
